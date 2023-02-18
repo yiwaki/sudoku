b44ee16814916db9b0730a2914dba59d54664699
Y.Iwaki <y_iwaki@me.com>
2023-02-18 17:37:17 +0900

    modified:   src/bruteforce.c 	modified:   src/bruteforce.h

--- src/bruteforce.h
+++ src/bruteforce.h
@@ -3,6 +3,6 @@
 
 #include "matrix.h"
 
-void bruteforce(matrix_t *const x, int n, matrix_t *const y);
+void bruteforce(const matrix_t *const x, int n, matrix_t *const y);
 
 #endif  // BRUTEFORCE_H
