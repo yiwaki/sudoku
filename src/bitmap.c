89c47df038c1cf832d1ee4da8227d21e082a7952
Y.Iwaki <y_iwaki@me.com>
2023-03-06 16:07:53 +0900

    modified:   src/bitmap.c 	modified:   src/bitmap.h 	modified:   src/bruteforce.c

--- src/bitmap.c
+++ src/bitmap.c
@@ -1,13 +1,14 @@
+#define __STDC_WANT_LIB_EXT1__ 1
 #include "bitmap.h"
 
 #include <string.h>
 
-int split_single_bit(bitmap_t bit, bitmap_t bits[]) {
+int split_single_bit(bitmap_t bit, int size, bitmap_t bits[]) {
     int cnt = 0;
 
-    memset(bits, 0, BITMAP_DIGIT * sizeof(bitmap_t));
+    memset_s(bits, size * sizeof(bitmap_t), 0, size * sizeof(bitmap_t));
 
-    for (int i = 0; i < BITMAP_DIGIT; i++) {
+    for (int i = 0; i < size; i++) {
         bitmap_t target_bit = 1 << i;
         if (bit & target_bit) {
             bits[cnt] = target_bit;
